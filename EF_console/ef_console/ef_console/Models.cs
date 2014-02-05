dcb4e66104ede6cf78f17b553a55f20006102dae
unknown <s00009509@LAB-B1042-15.campus.itsligo.ie>
2014-02-05 09:09:12 +0000

    update before class

--- EF_console/ef_console/ef_console/Models.cs
+++ EF_console/ef_console/ef_console/Models.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +10,58 @@ namespace ef_console
 {
     public class Blog
     {
+        [Key]
         public int BlogId { get; set; }
         public string Name { get; set; }
 
         public virtual List<Post> Posts { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 
     public class Post
     {
+        [Key]
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
 
         public int BlogId { get; set; }
         public virtual Blog Blog { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext (): base("BlogEntities"){}
+	    
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class BlogDBSeedDatabase : DropCreateDatabaseAlways<BloggingContext>
+    {
+        protected override void Seed(BloggingContext db)
+        {
+            Blog b = new Blog();
+            b.Name = "My Fourth Blog";
+            Post p = new Post();
+            p.Title = "My First Post";
+            p.Content = "First post in fourth blog";
+            b.Posts.Add(p);
+            
+            try
+            {
+                db.Posts.Add(p);
+                db.Blogs.Add(b);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);                
+            }
+        }
     }
 }
--- EF_console/ef_console/ef_console/Program.cs
+++ EF_console/ef_console/ef_console/Program.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace ef_console
 {
     class Program
-    {
+    {         
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            BloggingContext db = new BloggingContext();
+
+            //Blog b = new Blog();
+            //b.Name = "My Third Blog";
+            //Post p = new Post();
+            //p.Title = "My First Post";
+            //p.Content = "Testing";
+            //p.BlogId = 1;
+            //try
+            //{
+            //    db.Posts.Add(p);
+            //    db.SaveChanges();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);                
+            //}
+            
+
+            var q = db.Blogs;
+            
+            foreach (var blog in q)
+            {   
+                Console.WriteLine("{0}", blog);
+            }
+            Console.ReadLine();
         }
     }
 }
